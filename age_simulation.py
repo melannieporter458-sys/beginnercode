80594ec109047323511b47645386f6c01d8e89f1
melannieporter458-sys <melannieporter458@gmail.com>
2026-03-12 23:35:09 -0400

    Add files via upload

---
+++ age_simulation.py
@@ -0,0 +1,4 @@
+name=input("enter your name")
+birth_year=int(input("Enter your birth year"))
+age=2026-birth_year
+print("Hello", name,"!","your age is",age)
---
+++ number_loop.py
@@ -0,0 +1,4 @@
+count=1
+while count <=10:
+   print(count)
+   count +=1 #increase the number after each print
